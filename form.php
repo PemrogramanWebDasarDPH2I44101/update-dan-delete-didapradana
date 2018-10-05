e5f443d34b4ab7c18401c9f7e8465b8d75176faa
DidaPradana <42725223+didapradana@users.noreply.github.com>
2018-10-05 08:08:47 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,31 @@
-<!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
-</html>
+<form method="post">
+	Nama <input type="text" name="nama"><br>
+	NIM <input type="text" pattern="\d*" name="nim"><br>
+	Tanggal lahir <input type="date" name="tgl_lahir"><br><br>
+	<input type="submit" name="kirim">
+</form>
+<?php
+	include("conn2.php");
+
+	if(isset($_POST['kirim'])){
+		$nama = $_POST['nama'];
+		$nim = $_POST['nim'];
+		$tgl_lahir = $_POST['tgl_lahir'];
+
+		$sql = "INSERT INTO mahasiswa(nama, nim, tgl_lahir) 
+				VALUES ('$nama','$nim','$tgl_lahir')";
+
+		if (mysqli_query($conn, $sql)) {
+				echo "Barhasil Di Input";
+		}else{
+			echo "Error" .$sql."<br>". mysqli_error($conn);
+		}
+		mysqli_close($conn);
+		echo "<br>";
+		echo "<br>";
+		echo "Silahkan Klik link dibawah ini untuk melihat data <br>";
+		?>
+		<a href="list.php">View</a>
+		<?php
+	}
+?>
